cdae39ff1b2072aab9a2ddca930dc9b6af1d6c63
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-01 20:29:03 -0400

    Step 1-3

--- workshop/exercise-1/scripts.js
+++ workshop/exercise-1/scripts.js
@@ -1,1 +1,22 @@
 // 
+const backgroundYear = document.getElementById("year");
+const eighteen = document.getElementById("2018button");
+const nineteen = document.getElementById("2019button");
+
+
+window.addEventListener("load", ()=>{
+    eighteen.classList.add("active");
+    backgroundYear.innerText = eighteen.innerText
+})
+
+nineteen.addEventListener("click", ()=>{
+    nineteen.classList.add("active");
+    eighteen.classList.remove("active");
+    backgroundYear.innerText = nineteen.innerText
+})
+
+eighteen.addEventListener("click", ()=>{
+   eighteen.classList.add("active");
+    nineteen.classList.remove("active");
+    backgroundYear.innerText = eighteen.innerText
+})
